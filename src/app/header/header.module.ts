c3dd9cb7719255f312f92bd7ddc34b1148ada109
juan-kuhne <juankuhne@hotmail.com>
2023-11-11 14:27:20 -0300

    icons and navigations names

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {
+  bootstrapBook,
+  bootstrapBuildings,
   bootstrapEmojiWinkFill,
   bootstrapHouseDoor,
+  bootstrapListCheck,
 } from '@ng-icons/bootstrap-icons';
 import { NgIconsModule } from '@ng-icons/core';
 import { HeaderComponent } from './header.component';
 
-const icons = { bootstrapEmojiWinkFill, bootstrapHouseDoor };
+const icons = {
+  bootstrapEmojiWinkFill,
+  bootstrapHouseDoor,
+  bootstrapListCheck,
+  bootstrapBook,
+  bootstrapBuildings,
+};
 
 @NgModule({
   declarations: [HeaderComponent],
